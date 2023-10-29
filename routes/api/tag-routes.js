f5db9c308e086fb43720e58582a34c9676137d42
Jenae Luthi <jenaeluthi@gmail.com>
2023-10-29 14:44:01 -0600

    added model information for Tag, Product,ProductTag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,7 +6,7 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', async (req, res) => {
   try {
     const tagData = await Tag.findAll({
-      include: [{ model: Tag, Product, ProductTag }],
+      include: [{ model: Tag},{ model: Product}, { model: ProductTag }],
     });
     res.status(200).json(tagData);
   } catch (err) {
@@ -19,7 +19,7 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   try {
     const tagData = await Tag.findByPk(req.params.id, {
-      include: [{ model: Tag, Product, ProductTag }],
+      include: [{ model: Tag},{ model: Product}, { model: ProductTag }],
     });
 
     if (!tagData) {
